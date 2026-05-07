1bf8436a704e564c5f0006ce4811a1fcd98e2846
Samuel Andrade <samuelandrademedeiros@gmail.com>
2026-05-06 22:30:32 -0300

    feat: integrate SkillsGrid into page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { HeroSection } from "@/components/HeroSection";
 import { CoreEngine } from "@/components/CoreEngine";
+import { SkillsGrid } from "@/components/SkillsGrid";
 import { ProjectHangar } from "@/components/ProjectHangar";
 import { Terminal } from "@/components/Terminal";
 import { UtilityDeck } from "@/components/UtilityDeck";
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <HeroSection />
       <CoreEngine />
+      <SkillsGrid />
       <ProjectHangar />
       <Terminal />
       <UtilityDeck />
