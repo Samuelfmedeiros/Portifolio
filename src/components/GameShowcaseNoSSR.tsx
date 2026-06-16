e52c17e81fe03d264242291283913677d5990300
Samuel Medeiros <samuel@arachne.app>
2026-06-16 02:00:33 -0300

    auto-sync: 2026-06-16 02:00

--- src/components/GameShowcaseNoSSR.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const GameShowcaseNoSSR = dynamic(
-  () => import("@/components/GameShowcase").then((mod) => mod.GameShowcase),
-  { ssr: false }
-);
